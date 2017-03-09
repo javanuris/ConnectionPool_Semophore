cda3d4348f08ac1da26c52543d06b3d9b5e57cdb
User <User>
2017-03-09 21:49:50 +0600

    doatalk version

---
+++ src/main.nuris.epam/java/dao/AbstractDao.java
@@ -0,0 +1,37 @@
+package dao;
+
+import java.sql.Connection;
+import java.util.List;
+
+/**
+ * Created by User on 09.03.2017.
+ */
+public abstract class AbstractJDBCDao<T extends Identified<PK>,PK extends Integer> implements GenericDao<T , PK> {
+    Connection connection = null;
+    public AbstractJDBCDao(Connection connection){
+        this.connection = connection;
+    }
+    public T create() {
+        return null;
+    }
+
+    public T persist(T object) {
+        return null;
+    }
+
+    public T getByPK(PK key) {
+        return null;
+    }
+
+    public void update(T object) {
+
+    }
+
+    public void delete(T object) {
+
+    }
+
+    public List<T> getAll() {
+        return null;
+    }
+}
